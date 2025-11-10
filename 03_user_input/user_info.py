e01c531714fce704830a75a4002ae7902d150961
rezpoin <rezpoin@gmail.com>
2025-11-11 00:04:28 +0300

    Полная синхронизация локальной структуры с GitHub

--- 03_user_input/user_info.py
+++
@@ -1,8 +0,0 @@
-name=input("Введите своё имя: ")
-
-age=int(input("Введите свой возраст: "))
-
-greeting=f"Привет, {name}!"
-
-print(greeting)
-print(f"Тебе {age} лет")
--- 04_loops/while_example.py
+++
@@ -1,7 +0,0 @@
-number = -1
-
-while number != 0:
-    number = int(input("Введите число (0 для выхода): "))
-    print(f"Вы ввели {number}")
-
-print("Цикл завершён")
