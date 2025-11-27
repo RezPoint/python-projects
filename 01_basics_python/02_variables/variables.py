c5be0f9daec6958a0e03f49732f82cc361ae7eb1
rezpoin <rezpoin@gmail.com>
2025-11-27 10:38:10 +0300

    Добавить детали в урок о переменных

--- 01_basics_python/02_variables/variables.py
+++ 01_basics_python/02_variables/variables.py
@@ -16,3 +16,13 @@ print(greeting)
 print("Возраст:", age)
 print("Рост:", height, "м")
 print("Студент:", is_student)
+
+
+# Дополнительные факты
+favorite_language = "Python"
+hours_per_day = 8
+
+print(f"Мой любимый язык программирования: {favorite_language}")
+print(f"Я изучаю {favorite_language} {hours_per_day} часов в день")
+
+print(f"{name} учится {favorite_language}, чтобы устроиться в будущем на хорошую работу в IT!")
